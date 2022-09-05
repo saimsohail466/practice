328a83162720dcf7f173d7d014f53a5b2636d6b1
saimsohail466 <saimsohail466@gmail.com>
2022-09-05 15:37:55 +0500

    Added dashboard controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'dashboards/index'
+  root 'dashboards#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
